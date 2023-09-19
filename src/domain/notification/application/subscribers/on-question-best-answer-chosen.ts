b6f620786acfe03501cf3a5aa6256349566109d2
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-19 15:11:15 -0300

    refactor(on-question-best-answer-chosen.ts): update import statement for AnswersRepository to reflect correct file path

--- src/domain/notification/application/subscribers/on-question-best-answer-chosen.ts
+++ src/domain/notification/application/subscribers/on-question-best-answer-chosen.ts
@@ -1,12 +1,12 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { IEventHandler } from '@/core/events/event-handler'
-import { IAnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { QuestionBestAnswerChosenEvent } from '@/domain/forum/enterprise/events/question-best-answer-chosen-event'
 import { SendNotificationUseCase } from '@/domain/notification/application/use-cases/send-notification'
 
 export class OnQuestionBestAnswerChosen implements IEventHandler {
   constructor(
-    private answersRepository: IAnswersRepository,
+    private answersRepository: AnswersRepository,
     private sendNotification: SendNotificationUseCase,
   ) {
     this.setupSubscriptions()
