9ff7a49682d0b3f494601f11dcb81a8c2f6dd38a
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-22 18:31:14 -0300

    feat(storage): add R2Storage implementation for Uploader interface feat(storage): create StorageModule to provide R2Storage as Uploader implementation

---
+++ src/infra/storage/r2-storage.ts
@@ -0,0 +1,49 @@
+import {
+  IUploadParams,
+  Uploader,
+} from '@/domain/forum/application/storage/uploader'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { Injectable } from '@nestjs/common'
+import { randomUUID } from 'node:crypto'
+
+import { EnvService } from '../env/env.service'
+
+@Injectable()
+export class R2Storage implements Uploader {
+  private client: S3Client
+
+  constructor(private envService: EnvService) {
+    const accountId = envService.get('CLOUDFLARE_ACCOUNT_ID')
+
+    this.client = new S3Client({
+      endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+      region: 'auto',
+      credentials: {
+        accessKeyId: envService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: envService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    })
+  }
+
+  async upload({
+    fileName,
+    fileType,
+    body,
+  }: IUploadParams): Promise<{ url: string }> {
+    const uploadId = randomUUID()
+    const uniqueFileName = `${uploadId}-${fileName}`
+
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: this.envService.get('AWS_BUCKET_NAME'),
+        Key: uniqueFileName,
+        ContentType: fileType,
+        Body: body,
+      }),
+    )
+
+    return {
+      url: uniqueFileName,
+    }
+  }
+}
