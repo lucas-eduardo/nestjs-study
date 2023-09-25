cac79ba2467af70890b035866bbc5b6c772f8d32
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-25 13:56:47 -0300

    fix(make-question-attachments.ts): fix import statements and add missing Injectable decorator feat(make-question-attachments.ts): add QuestionAttachmentFactory class with makePrismaQuestionAttachment method to create and update question attachments in Prisma database

--- test/factories/make-question-attachments.ts
+++ test/factories/make-question-attachments.ts
@@ -3,6 +3,8 @@ import {
   QuestionAttachment,
   IQuestionAttachmentProps,
 } from '@/domain/forum/enterprise/entities/question-attachment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestionAttachment(
   override: Partial<IQuestionAttachmentProps> = {},
@@ -18,4 +20,26 @@ export function makeQuestionAttachment(
   )
 
   return questionAttachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<IQuestionAttachmentProps> = {},
+  ): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString(),
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
