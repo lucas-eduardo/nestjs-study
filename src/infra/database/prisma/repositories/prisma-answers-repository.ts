15e6a6ab5efe9c64b704b5b1000e2798e329263c
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-25 14:09:30 -0300

    fix(prisma-answers-repository.ts): add AnswerAttachmentsRepository dependency to the constructor to enable creating and deleting answer attachments feat(prisma-answers-repository.ts): add logic to create and delete answer attachments when creating and saving answers respectively

--- src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -1,4 +1,5 @@
 import { IPaginationParams } from '@/core/repositories/pagination-params'
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { Injectable } from '@nestjs/common'
@@ -8,7 +9,10 @@ import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaAnswersRepository implements AnswersRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private answerAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
 
   async findById(id: string): Promise<Answer | null> {
     const answer = await this.prisma.answer.findUnique({ where: { id } })
@@ -42,17 +46,29 @@ export class PrismaAnswersRepository implements AnswersRepository {
     const data = PrismaAnswerMapper.toPrisma(answer)
 
     await this.prisma.answer.create({ data })
+
+    await this.answerAttachmentsRepository.createMany(
+      answer.attachments.getItems(),
+    )
   }
 
   async save(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer)
 
-    await this.prisma.answer.update({
-      where: {
-        id: data.id,
-      },
-      data,
-    })
+    await Promise.all([
+      this.prisma.answer.update({
+        where: {
+          id: answer.id.toString(),
+        },
+        data,
+      }),
+      this.answerAttachmentsRepository.createMany(
+        answer.attachments.getNewItems(),
+      ),
+      this.answerAttachmentsRepository.deleteMany(
+        answer.attachments.getRemovedItems(),
+      ),
+    ])
   }
 
   async delete(answer: Answer): Promise<void> {
