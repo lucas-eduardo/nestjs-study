2431e7cbd7b03493b3a2625fd0b9158bef391b9e
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-25 13:50:34 -0300

    feat(make-attachment.ts): add factory function to create Attachment entities and a class to create and persist Attachment entities in the Prisma database
    The `makeAttachment` function is added to create Attachment entities with optional overrides and an optional UniqueEntityID. This function uses the `Attachment.create` method to create the entity.
    
    The `AttachmentFactory` class is added to provide a way to create and persist Attachment entities in the Prisma database. It has a `makePrismaAttachment` method that takes partial data for an Attachment entity and creates the entity using the `makeAttachment` function. It then uses the `prisma.attachment.create` method to persist the entity in the database.

---
+++ test/factories/make-attachment.ts
@@ -0,0 +1,42 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Attachment,
+  IAttachmentProps,
+} from '@/domain/forum/enterprise/entities/attachment'
+import { PrismaAttachmentMapper } from '@/infra/database/prisma/mappers/prisma-attachment-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
+
+export function makeAttachment(
+  override: Partial<IAttachmentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const attachment = Attachment.create(
+    {
+      title: faker.lorem.slug(),
+      url: faker.lorem.slug(),
+      ...override,
+    },
+    id,
+  )
+
+  return attachment
+}
+
+@Injectable()
+export class AttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAttachment(
+    data: Partial<IAttachmentProps> = {},
+  ): Promise<Attachment> {
+    const attachment = makeAttachment(data)
+
+    await this.prisma.attachment.create({
+      data: PrismaAttachmentMapper.toPrisma(attachment),
+    })
+
+    return attachment
+  }
+}
