c0c04bef44d924a819ebaa05c1b31146f1ca1ebc
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-25 14:08:27 -0300

    fix(make-answer-attachments.ts): remove unused imports and injectable decorator feat(make-answer-attachments.ts): add AnswerAttachmentFactory class with makePrismaAnswerAttachment method to create and update answer attachments in Prisma

--- test/factories/make-answer-attachments.ts
+++ test/factories/make-answer-attachments.ts
@@ -3,6 +3,8 @@ import {
   AnswerAttachment,
   IAnswerAttachmentProps,
 } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswerAttachment(
   override: Partial<IAnswerAttachmentProps> = {},
@@ -18,4 +20,26 @@ export function makeAnswerAttachment(
   )
 
   return answerAttachment
+}
+
+@Injectable()
+export class AnswerAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerAttachment(
+    data: Partial<IAnswerAttachmentProps> = {},
+  ): Promise<AnswerAttachment> {
+    const answerAttachment = makeAnswerAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: answerAttachment.attachmentId.toString(),
+      },
+      data: {
+        answerId: answerAttachment.answerId.toString(),
+      },
+    })
+
+    return answerAttachment
+  }
 }
