e5c2842f9c2e80d7c6210443b3e32ff5e031afea
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-27 13:22:58 -0300

    test(get-question-by-slug.spec.ts): add test case to get a question by slug feat(get-question-by-slug.spec.ts): add support for retrieving question attachments and author information in the result fix(get-question-by-slug.spec.ts): fix missing author information in the result

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,11 +1,18 @@
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { makeAttachment } from 'test/factories/make-attachment'
 import { makeQuestion } from 'test/factories/make-question'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachments'
+import { makeStudent } from 'test/factories/make-student'
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
 import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
 
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
 
@@ -13,18 +20,42 @@ describe('Get Question By Slug', () => {
   beforeEach(() => {
     inMemoryQuestionAttachmentsRepository =
       new InMemoryQuestionAttachmentsRepository()
+
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentsRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository,
     )
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
   })
 
   it('should be able to get a question by slug', async () => {
+    const student = makeStudent({ name: 'John Doe' })
+
+    await inMemoryStudentsRepository.create(student)
+
     const newQuestion = makeQuestion({
+      authorId: student.id,
       slug: Slug.create('example-question'),
     })
 
     await inMemoryQuestionsRepository.create(newQuestion)
+
+    const attachment = makeAttachment({
+      title: 'Some attachment',
+    })
+
+    inMemoryAttachmentsRepository.items.push(attachment)
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        attachmentId: attachment.id,
+        questionId: newQuestion.id,
+      }),
+    )
 
     const result = await sut.execute({
       slug: 'example-question',
@@ -33,6 +64,12 @@ describe('Get Question By Slug', () => {
     expect(result.value).toMatchObject({
       question: expect.objectContaining({
         title: newQuestion.title,
+        author: 'John Doe',
+        attachments: [
+          expect.objectContaining({
+            title: attachment.title,
+          }),
+        ],
       }),
     })
   })
