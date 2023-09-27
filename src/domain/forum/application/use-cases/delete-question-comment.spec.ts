a9377ddf7fd9aa7f82c16d77c751d88aa3d414e9
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-27 13:22:17 -0300

    test(delete-question-comment.spec.ts): add support for InMemoryStudentsRepository in InMemoryQuestionCommentsRepository to improve test coverage and functionality

--- src/domain/forum/application/use-cases/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -3,14 +3,18 @@ import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
 import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let sut: DeleteQuestionCommentUseCase
 
 describe('Delete Question Comment', () => {
   beforeEach(() => {
-    inMemoryQuestionCommentsRepository =
-      new InMemoryQuestionCommentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository(
+      inMemoryStudentsRepository,
+    )
 
     sut = new DeleteQuestionCommentUseCase(inMemoryQuestionCommentsRepository)
   })
