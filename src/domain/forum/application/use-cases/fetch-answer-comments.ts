617bd629d7e206c1abeae47cb3d9bffcd2c9459c
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-27 13:08:03 -0300

    refactor(fetch-answer-comments.ts): rename 'answerComments' variable to 'comments' for better clarity and consistency feat(fetch-answer-comments.ts): update method call to 'findManyByAnswerIdWithAuthor' to retrieve comments with author information

--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,7 +1,8 @@
 import { Either, right } from '@/core/either'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
-import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 import { Injectable } from '@nestjs/common'
+
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 interface IFetchAnswerCommentsUseCaseRequest {
   answerId: string
@@ -11,7 +12,7 @@ interface IFetchAnswerCommentsUseCaseRequest {
 type FetchAnswerCommentsUseCaseResponse = Either<
   null,
   {
-    answerComments: AnswerComment[]
+    comments: CommentWithAuthor[]
   }
 >
 
@@ -23,13 +24,16 @@ export class FetchAnswerCommentsUseCase {
     answerId,
     page,
   }: IFetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
-    const answerComments =
-      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
-        page,
-      })
+    const comments =
+      await this.answerCommentsRepository.findManyByAnswerIdWithAuthor(
+        answerId,
+        {
+          page,
+        },
+      )
 
     return right({
-      answerComments,
+      comments,
     })
   }
 }
