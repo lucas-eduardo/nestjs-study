52d6f94aa6d99dd58bff9dccc87b10e8a0df750a
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-27 13:14:51 -0300

    feat(question-details.ts): add QuestionDetails value object
    This commit adds the `QuestionDetails` value object to the `src/domain/forum/enterprise/entities/value-objects` directory. The `QuestionDetails` value object represents the details of a question in the forum. It contains properties such as `questionId`, `authorId`, `author`, `title`, `content`, `slug`, `attachments`, `bestAnswerId`, `createdAt`, and `updatedAt`. This value object is used to encapsulate and provide access to the details of a question in a type-safe manner.

---
+++ src/domain/forum/enterprise/entities/value-objects/question-details.ts
@@ -0,0 +1,64 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ValueObject } from '@/core/entities/value-object'
+
+import { Attachment } from '../attachment'
+import { Slug } from './slug'
+
+export interface IQuestionDetailsProps {
+  questionId: UniqueEntityID
+  authorId: UniqueEntityID
+  author: string
+  title: string
+  content: string
+  slug: Slug
+  attachments: Attachment[]
+  bestAnswerId?: UniqueEntityID | null
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class QuestionDetails extends ValueObject<IQuestionDetailsProps> {
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get author() {
+    return this.props.author
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get attachments() {
+    return this.props.attachments
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(props: IQuestionDetailsProps) {
+    return new QuestionDetails(props)
+  }
+}
