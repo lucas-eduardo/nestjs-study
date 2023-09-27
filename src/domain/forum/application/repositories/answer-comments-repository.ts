b8dcd67419ef6626f90dd91c32b9bde058f03c02
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-27 13:07:27 -0300

    feat(answer-comments-repository.ts): add new method findManyByAnswerIdWithAuthor to retrieve answer comments with author information
    The `findManyByAnswerIdWithAuthor` method is added to the `AnswerCommentsRepository` interface. This new method allows retrieving answer comments along with the author information. This is useful when displaying answer comments in the UI, as it provides additional context about the comment author.

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,12 +1,19 @@
 import { IPaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
+
 export abstract class AnswerCommentsRepository {
   abstract findById(id: string): Promise<AnswerComment | null>
   abstract findManyByAnswerId(
     answerId: string,
     params: IPaginationParams,
   ): Promise<AnswerComment[]>
+
+  abstract findManyByAnswerIdWithAuthor(
+    answerId: string,
+    params: IPaginationParams,
+  ): Promise<CommentWithAuthor[]>
 
   abstract create(answerComment: AnswerComment): Promise<void>
   abstract delete(answerComment: AnswerComment): Promise<void>
