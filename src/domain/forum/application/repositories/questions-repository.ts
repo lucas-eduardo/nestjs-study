b0693c76194e3dbe4b3f8e1f2a971e8c0e71cec1
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-27 13:15:04 -0300

    feat(questions-repository.ts): add method findDetailsBySlug to retrieve question details by slug
    The method findDetailsBySlug is added to the QuestionsRepository abstract class. This method allows retrieving question details by slug. This is useful when more detailed information about a question is needed, such as the author, creation date, and other related data.

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,12 @@
 import { IPaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
+
 export abstract class QuestionsRepository {
   abstract findById(id: string): Promise<Question | null>
   abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract findManyRecent(params: IPaginationParams): Promise<Question[]>
   abstract save(question: Question): Promise<void>
   abstract create(question: Question): Promise<void>
